e9b1f26d42875084fb47020fe3e32c6ff612e0e1
Vincent Smuda <vincent@vincentsmuda.com>
2018-09-08 18:20:22 -0600

    Creates a js config

---
+++ source/scripts/bootstrap/config.js
@@ -0,0 +1,14 @@
+window.sambar = {
+
+    // Holds the vue instance
+    vue: null,
+
+    // General configurations
+    config: {
+
+        // Sets the duration of the loading/page transition
+        loading_transition_duration: 2000
+
+    }
+
+};
