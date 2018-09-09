84fede35496a715845d015900ac81d34045243b3
Vincent Smuda <vincent@vincentsmuda.com>
2018-09-08 18:18:32 -0600

    Adds resolve for the source directory

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,9 @@ const theme_name = 'sambar',
 module.exports = {
 
 	// What's going in
-	entry: 'source/scripts/entry.js',
+	entry: path.resolve(__dirname, 'source/scripts/entry.js'),
+
+	mode: 'development',
 
 	// What's happening to it
 	module: {
@@ -111,6 +113,14 @@ module.exports = {
 		publicPath: 'js/',
 		chunkFilename: '[name].bundle.js',
 		filename: `${theme_name}.js`
+	},
+
+	// Set up aliases
+	resolve: {
+		alias: {
+			vue: 'vue/dist/vue.js',
+			'@': path.resolve(__dirname, 'source')
+		}
 	},
 
 	// Plugins that will help us add functionality to webpack
