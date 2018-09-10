d7a28e364601512737eec5b8821550b0aaa73a56
Vincent Smuda <vincent@vincentsmuda.com>
2018-09-10 16:34:29 -0600

    Abstracts transition callbacks and adds stores (loading)

--- source/scripts/bootstrap/vue.js
+++ source/scripts/bootstrap/vue.js
@@ -1,6 +1,9 @@
-import Vue from 'vue';
+import { mapGetters } from 'vuex'
+import '@/scripts/components/_components';
 import VueRouter from 'vue-router';
 import router from './router';
+import store from './vuex';
+import Vue from 'vue';
 
 // Make sure vue uses vuerouter
 Vue.use(VueRouter);
@@ -17,9 +20,7 @@ window.sambar.vue = new Vue({
      * Root data
      * @return  {Object}
      */
-    data: () => ({
-        loading: true
-    }),
+    data: () => ({}),
 
     /**
      * Triggers when the root vue component renders
@@ -29,7 +30,57 @@ window.sambar.vue = new Vue({
 
         // Make sure that everythin has loaded before
         // removing the loading state
-        setTimeout(() => this.loading = false, 500);
+        setTimeout(
+            () => this.$store.dispatch('loading/finishLoading'),
+            500
+        );
+
+    },
+
+    /**
+     * Methods attached to the vue instance
+     * @return  {Objects}
+     */
+    methods: {
+
+        /**
+         * Runs before the new content loads
+         * @return  {Objects}
+         */
+        beforeRender () {
+
+            // Trigger the loading state
+            this.$store.dispatch('loading/startLoading');
+
+            // Run any router mounting logic
+            window.sambar.behaviours.destroy();
+
+        },
+
+        /**
+         * Runs after the new content loads
+         * @return  {Objects}
+         */
+        afterRender () {
+
+            // Turn the loading off
+            this.$store.dispatch('loading/finishLoading');
+
+            // Run any router mounting logic
+            window.sambar.behaviours.mount();
+
+        }
+
+    },
+
+    /**
+     * Computed props
+     * @return  {Objects}
+     */
+    computed: {
+
+        // expands the loading state getters
+        ...mapGetters('loading', ['is_loading'])
 
     },
 
@@ -37,6 +88,12 @@ window.sambar.vue = new Vue({
      * Mount router
      * @return  {VueRouter}
      */
-    router
+    router,
+
+    /**
+     * The router's store
+     * @return  {Vuex}
+     */
+    store
 
 });
