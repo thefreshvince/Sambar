c3e49453c4b32441cabff9086b9e9ef1237120ff
Vincent Smuda <vincent@vincentsmuda.com>
2018-09-10 16:38:49 -0600

    Adds query string args to the generated route components

--- source/scripts/lib/router/updateRoute.js
+++ source/scripts/lib/router/updateRoute.js
@@ -10,30 +10,30 @@ export default (to, routes) => new Promise(resolve => {
     setTimeout(() => {
         
         // if it does not exist, add it!
-        if(!routes.some(r => r.path == to.path))
-            getPage(to.path).then(contents => {
+        if(!to.matched.length) getPage(to.fullPath).then(contents => {
 
-                // Store the new route as a component
-                let new_route = {
-                    path: to.path, 
-                    meta: contents.meta,
-                    component: {
-                        template: contents.template
-                    }
-                };
+            // Store the new route as a component
+            let new_route = {
+                path: to.path, 
+                meta: contents.meta,
+                query: to.query,
+                component: {
+                    template: contents.template
+                }
+            };
 
-                // Add the route to the router instance
-                // ** attached to the vue instance
-                window.sambar.vue.$router.addRoutes([new_route]);
+            // Add the route to the router instance
+            // ** attached to the vue instance
+            window.sambar.vue.$router.addRoutes([new_route]);
 
-                // Add the new route to the routes array
-                // For checking later
-                routes.push(new_route);
-                
-                // Now try navigating to it again
-                resolve(to.path);
+            // Add the new route to the routes array
+            // For checking later
+            routes.push(new_route);
+            
+            // Now try navigating to it again
+            resolve(to.fullPath);
 
-            });
+        });
 
         // It exists so just continue on
         else resolve(undefined);
